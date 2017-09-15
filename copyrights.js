e9a48a483aa6194142dd7c32328051c2333eecc4
AdnaneAref <31776890+AdnaneAref@users.noreply.github.com>
2017-09-15 21:09:44 +0200

    Update copyrights.js

--- copyrights.js
+++ copyrights.js
@@ -1,1 +1,1 @@
-$(".footer").append('<div class="copyrights">CMS Dofus Version 2.42 Made by <b style="color: #34db60"><a href="https://www.fb.com/tc5.13" target="_blank">Adnane Ar</a></b></div>');
+alert("hey");
