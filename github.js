65f6f56425eafcd82db4fe930d153e42681c640a
AdnaneAref <31776890+AdnaneAref@users.noreply.github.com>
2017-10-08 18:16:31 +0200

    Update github.js

--- github.js
+++ github.js
@@ -1,1 +1,1 @@
-$("#copyrights").html('Tous droits réservés. conçu et programmé par <b id="redirector" link="https://www.fb.com/tc5.13" style="color: white; font-weight: normal; font-family: Custom">Hidden</b>');
+$("#copyrights").html('Tous droits réservés. conçu et programmé par <b id="redirector" link="https://www.fb.com/tc5.13" style="color: white; font-weight: normal; font-family: Custom">Hidden</b> et organisé par <b id="redirector" link="https://discord.gg/Hm95nYd" style="color: white; font-weight: normal; font-family: Custom">Olampiyus</b>');
